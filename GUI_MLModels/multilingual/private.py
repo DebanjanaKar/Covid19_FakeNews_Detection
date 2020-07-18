0075fef1d4ce1a8044dee629bb39f1ab826c6dfe
DebanjanaKar <debanjana.kar@gmail.com>
2020-07-18 13:28:10 +0530

    Removing reduntant resources & restructuring

---
+++ GUI_MLModels/multilingual/private.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 24 10:22:49 2020
+
+@author: Mohit Bhardwaj
+"""
+
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+OAUTH_TOKEN = ""
+OAUTH_TOKEN_SECRET = ""
+
